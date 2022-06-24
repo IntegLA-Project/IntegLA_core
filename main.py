8ecc1c571c9e8ad4af3cb74b7f09c70fb6c774fa
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2022-06-25 03:46:16 +0900

    dir create

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
-import clib_gen
-# obj_dir="obj/"
+import os
+import shutil
+from clib_gen import clib_gen
+
+obj_dir="./obj/"
+if(os.path.isdir(obj_dir) == True):
+    shutil.rmtree(obj_dir)
+os.mkdir(obj_dir)
 # setup_dir(objdir)
  
-with clib_gen.reading_incompletely('axpy.c', "axpy_test.cpp", "blas.hpp" ) as f:
+with clib_gen.reading_incompletely(obj_dir+"axpy.c", obj_dir+"axpy_test.cpp", obj_dir+"blas.hpp" ) as f:
     clib_gen.generate(func = "axpy",
         targets = [("double", "void"), ("float", "void"), ("int32_t", "void"), ("int64_t", "void")],
         args = [("{target}", "{Val0}"), ("const {Vec}", "{Vec0}"), ("{Vec}", "{Vec1}")],
